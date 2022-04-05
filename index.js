c637c713f95ebbea1b6b4c18b1cb7c709b97acd5
Ram Goel <rgoel766@gmail.com>
2022-04-05 13:47:12 +0530

    Recreated Pages with Bootstrap

--- index.js
+++ index.js
@@ -5,54 +5,47 @@ const ejs = require('ejs')
 const form = require('formidable')
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Users")
+// mongoose.connect("mongodb://localhost:27017/Users")
 
-// Set EJS as templating engine
-app.set('view engine', 'ejs');
 app.use(express.urlencoded({
     extended: true
 }));
 
+app.use(express.static(__dirname+'/views'));
+app.use(express.static(__dirname+'/css'));
+app.use(express.static(__dirname+'/js'));
+app.use(express.static(__dirname+'/images'));
 
-var RegSchema = new mongoose.Schema({
-    name: String,
-    bname: String,
-    num: Number,
-    email: String,
-    locate: String,
-    passw: String
-});
-
-
-
-var User = mongoose.model("User", RegSchema);
-
-
-app.use(express.static('css'));
+// var RegSchema = new mongoose.Schema({
+//     name: String,
+//     bname: String,
+//     num: Number,
+//     email: String,
+//     locate: String,
+//     passw: String
+// });
 
 
 
-app.get('/', (req, res) => {
-    res.render('index')
-});
+// var User = mongoose.model("User", RegSchema);
 
-app.get('/login', (req, res) => {
-    res.render('login', {
-        data: 'Login Now'
-    })
-});
-app.post('/loginPost', async (req, res) => {
+
+
+
+
+
+app.post('/loginPost', (req, res) => {
     console.log(req.body)
     
-    var log=await User.updateOne({ 'email': req.body.email },{'name':'mansi'});
-    var log2=await User.findOne({'name':'mansi'});
+    // var log=await User.updateOne({ 'email': req.body.email },{'name':'mansi'});
+    // var log2=await User.findOne({'name':'mansi'});
+     
     
-    
-    console.log(log)
+  
     
     if (req.body.email == "register@123") {
-        if (req.body.passw == 1234) {
-            res.render('Dash')
+        if (req.body.passw =='shiv') {
+            res.json
             //Add items with database in this line
         } else {
             res.send('Incorrect Credentials');
@@ -61,9 +54,6 @@ app.post('/loginPost', async (req, res) => {
      
 });
 
-app.get('/register', (req, res) => {
-    res.render('register')
-});
 
 app.post('/registerPost', (req, res) => {
     console.log(req.body)
@@ -71,20 +61,18 @@ app.post('/registerPost', (req, res) => {
     console.log(len)
     const mail = req.body.email.split('.');
 
-    var myData = new User(req.body);
+    // var myData = new User(req.body);
     if (len == 10) {
         if (mail[1] == "com") {
 
-            //Data Saving 
-            myData.save()
-                .then(item => {
-                    res.render('login', {
-                        data: "Registration successful! Login Now"
-                    })
-                })
-                .catch(err => {
-                    res.status(400).send("unable to save to database");
-                });
+            // //Data Saving 
+            // myData.save()
+            //     .then(item => {
+                    res.send("Success")
+                // })
+                // .catch(err => {
+                //     res.status(400).send("unable to save to database");
+                // });
 
 
         } else {
@@ -107,15 +95,7 @@ app.post('/registerPost', (req, res) => {
 });
 
 
-app.get('/prod', (req, res) => {
 
-    res.render('Product') //Add product Details from database
-});
-app.get('/order', (req, res) => {
-    res.render('BookOrder', {
-        message: "Place you Order Now"
-    })
-});
 app.post('/orderPost', (req, res) => {
 
     const len = req.body.num.toString().length;
@@ -135,17 +115,6 @@ app.post('/orderPost', (req, res) => {
     }
 });
 
-app.get('/addprod', (req, res) => {
-    res.render('AddProd', {
-        message: "Roll on Customers Now"
-    })
-});
-app.get('/mngprod', (req, res) => {
-    res.render('ManageProd')
-});
-app.get('/profile', (req, res) => {
-    res.render('SellProfile')
-});
 
 
 app.post('/prodPost', (req, res) => {
@@ -160,26 +129,6 @@ app.post('/prodPost', (req, res) => {
         });
     }
 });
-
-
-app.get('/editProd', (req, res) => {
-    res.render('AddProd', {
-        message: "Change Product Details"
-    })
-});
-
-app.get('/dltProd', (req, res) => {
-    res.send('Product Deleted');
-});
-app.get('/editProfile', (req, res) => {
-    res.send('This feature is currently Unavailible');
-});
-
-
-
-
-
-
 
 
 
---
+++ js/handler.js
@@ -0,0 +1,19 @@
+function handleReg(){
+   alert("Successfully Registered, Login Now")
+}
+
+function logOut(){
+   alert("Successfully Logged Out")
+}
+
+function openDrop(){
+   document.getElementById('notiDrop').classList.toggle("d-none")
+}
+
+function showCart(){
+   document.getElementById('cart-popup').classList.toggle('d-none')
+}
+
+function showFilter(){
+   document.getElementById('filterList').classList
+}
